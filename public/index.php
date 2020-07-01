9fcdff59ca8996a0963610f35f2bd8bd3670dbc4
DenisDorogov <denisdorogov@yandex.ru>
2020-07-01 18:06:14 +0500

    +

--- public/index.php
+++ public/index.php
@@ -4,11 +4,14 @@ spl_autoload_register(function ($classname) {
     require_once ($classname.'.php');
 });
 
-$text = "Собрание в 9:00";
+// 1 задание.
+//$text = "Собрание в 9:00";
+//$notification = new Message($text);
+//$notification = new MessageEmail($notification);
+//$notification = new MessageTelegram($notification);
+//$notification = new MessageWhatsApp($notification);
+//$notification->sendMessage();
 
-$notification = new Message($text);
-$notification = new MessageEmail($notification);
-$notification = new MessageTelegram($notification);
-$notification = new MessageWhatsApp($notification);
 
-$notification->sendMessage();
+// 2 задание.
+
