c7684215eccf39057f5173312cf633c027b22ca7
DenisDorogov <denisdorogov@yandex.ru>
2020-07-01 15:04:01 +0500

    Подготовился к работе.

--- public/index.php
+++ public/index.php
@@ -1,29 +1,13 @@
 <?php
-require '../config/const.php';
-require  '../';
-require '../Logistic/Logistic.php';
-require '../Logistic/RoadLogistic.php';
-require '../Logistic/ShipLogistic.php';
 
-require '../Product/Transport.php';
-require '../Product/Car.php';
-require '../Product/Ship.php';
+spl_autoload_register(function ($classname) {
+    require_once ($classname.'.php');
+});
 
 
-function test(Logistic $logistic)
+function test()
 {
-		//что-то происходит
-	$logistic->startDelivery();
 
-	return;
 }
 
-test(new ShipLogistic());
-
-
-// MySQLFactory, PostgreSQLFactory, OracleFactory
-
-
-//DBConnection - соединение с базой данных
-//DBRecord - запись таблицы
-//DBQueryBuilder - конструктор запросов.
+test();
