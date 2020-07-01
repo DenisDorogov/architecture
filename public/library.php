1d91912c3fe1963a7bab5205fb2aefc3c7b99e85
DenisDorogov <denisdorogov@yandex.ru>
2020-07-01 18:06:44 +0500

    коррекция формулы

--- public/library.php
+++ public/library.php
@@ -3,7 +3,7 @@ class CircleAreaLib
 {
     public function getCircleArea(int $diagonal)
     {
-        $area = (M_PI * $diagonal**2))/4;
+        $area = (M_PI * $diagonal**2)/4;
 
    return $area;
 }
