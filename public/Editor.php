131e46ab62b1013cb25d3e41d265935b5d0e3d01
DenisDorogov <denisdorogov@yandex.ru>
2020-07-20 00:56:09 +0500

    Реализовал метод cut

--- public/Editor.php
+++ public/Editor.php
@@ -3,12 +3,14 @@
 
 class Editor
 {
-    public function cut($file, $start, $stop)
+    public function cut($file, $start, $stop): void
     {
-
+        $temp = file_get_contents($file);
+        substr_replace($temp, '', $start, $stop - $start);
+        file_put_contents($file, $temp);
     }
 
-    public function paste($file, $data, $start)
+    public function paste($file, $data, $start): void
     {
         file_put_contents($file, $data, $start);
     }
