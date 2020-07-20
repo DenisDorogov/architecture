272dc17e0fb90342dfd50843c69749b2a9f2e479
DenisDorogov <denisdorogov@yandex.ru>
2020-07-20 21:59:22 +0500

    Доделаны методы gut, paste.

--- public/Editor.php
+++ public/Editor.php
@@ -6,12 +6,13 @@ class Editor
     public function cut($file, $start, $stop): void
     {
         $temp = file_get_contents($file);
-        substr_replace($temp, '', $start, $stop - $start);
+        $temp = substr_replace($temp, '', $start, $stop - $start);
         file_put_contents($file, $temp);
     }
 
     public function paste($file, $data, $start): void
-    {
-        file_put_contents($file, $data, $start);
+    {   $temp = file_get_contents($file);
+        $temp = substr_replace($temp, $data, $start, 0);
+        file_put_contents($file, $temp, $start);
     }
 }
