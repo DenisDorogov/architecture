3b7c5c80af16365aba93c759c8a5010fe096b9c7
DenisDorogov <denisdorogov@yandex.ru>
2020-07-20 23:37:19 +0500

    Добавил запись вырезанных объектов в массив.

--- public/Editor.php
+++ public/Editor.php
@@ -3,11 +3,13 @@
 
 class Editor
 {
-    public function cut($file, $start, $stop): void
+    public function cut($file, $start, $stop): string
     {
         $temp = file_get_contents($file);
+        $buffer = substr($temp, $start, $stop - $start);
         $temp = substr_replace($temp, '', $start, $stop - $start);
         file_put_contents($file, $temp);
+        return $buffer;
     }
 
     public function paste($file, $data, $start): void
