29814af12365434506bc65ea0f82a122dc486687
DenisDorogov <denisdorogov@yandex.ru>
2020-07-26 21:12:14 +0500

    Добавлены тестовые вызовы.

--- public/index.php
+++ public/index.php
@@ -3,17 +3,19 @@ require_once ('const.php');
 spl_autoload_register(function ($classname) {
     require_once ($classname.'.php');
 });
-
-//$command = new EditCommand(new Editor());
-//
-//
-//$command->cutText(0 , 5);
-//$command->pasteText('test0test1' , 0);
 file_put_contents('text.txt', '111222333444555666');
+
 $user = new User();
 
 $user->runCommand('cut', 3, 6);
 $user->runCommand('paste', 12);
+$user->runCommand('undo');
+$user->runCommand('undo');
+$user->runCommand('undo');
+$user->runCommand('redo');
+$user->runCommand('redo');
+$user->runCommand('redo');
+
 
 
 
